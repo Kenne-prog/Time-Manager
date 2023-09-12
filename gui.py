d79f961d625dd3cf12060cea6c518217ddd1b85e
Kenneth <nonveragain12345@gmail.com>
2023-09-12 15:21:57 -0700

    designed the ui and implemented the calender

--- gui.py
+++ gui.py
@@ -1,22 +1,33 @@
 import tkinter as tk
+import calendar
 
 class TaskManagerApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Task Manager")
 
-        # Create UI elements and set up the GUI
-        # ...
-        self.count_label = tk.Label(root, text="Count: 0")
-        self.count_label.pack()
-        
-        count = 0
-        # Create a button
-        self.button = tk.Button(root, text="Click Me", command=self.increment_count)
-        self.button.pack()  # Place the button in the window using pack()
-        self.count = 0
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
+        self.root.geometry(f"{int(screen_width * 0.5)}x{int(screen_height * 0.5)}+{(screen_width - int(screen_width * 0.5)) // 2}+{(screen_height - int(screen_height * 0.5)) // 2}")
 
-    def increment_count(self):
-        # Increment the count and update the label text
-        self.count += 1
-        self.count_label.config(text=f"Count: {self.count}")
+        
+        # Create a frame to hold the calendar
+        self.calendar_frame = tk.Frame(root)
+        self.calendar_frame.pack(fill=tk.BOTH, expand=True)  # Fill and expand with the window
+
+        # Create a calendar object for the current month
+        self.cal = calendar.monthcalendar(2023, 1)  # Replace with the desired year and month
+
+        # Create a grid of labels to represent each day
+        for row, week in enumerate(self.cal):
+            for col, day in enumerate(week):
+                if day == 0:
+                    continue
+                label = tk.Label(self.calendar_frame, text=str(day),anchor = "nw", padx=10, pady=10, borderwidth=1, relief="solid")
+                label.grid(row=row, column=col, sticky="nsew")  # Sticky makes labels expand with the grid cell
+
+        # Configure row and column weights for the calendar frame
+        for i in range(7):  # Assuming 7 columns for the days of the week
+            self.calendar_frame.columnconfigure(i, weight=1)
+        for i in range(len(self.cal) + 1):  # Adjust for the number of rows in the calendar
+            self.calendar_frame.rowconfigure(i, weight=1)
